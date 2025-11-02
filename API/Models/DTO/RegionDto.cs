f1084aab5da4a849d2236d99419e0a6d7e3c21d5
Hoang Minh Duc <hoangduc.kor@gmail.com>
2025-11-02 22:21:09 +0700

    học DTO

---
+++ API/Models/DTO/RegionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Models.DTO
+{
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        // dấu hỏi chấm cho phép truyền vào null
+        public string? RegionImageUrl { get; set; }
+    }
+}
